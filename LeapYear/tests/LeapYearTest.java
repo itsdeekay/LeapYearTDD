13a4b82feb7472011bfbf48219e30d1092029cca
Deepak Kumar <deepjindl65@gmail.com>
2020-05-15 20:57:49 +0530

    Year divide by 400

--- LeapYear/tests/LeapYearTest.java
+++ LeapYear/tests/LeapYearTest.java
@@ -2,7 +2,13 @@ package tests;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
+import src.LeapYear;
 
 public class LeapYearTest {
-    
+    private final LeapYear leapYear = new LeapYear();
+
+    @Test
+    void yearsDivisbleBy400() {
+        assertEquals(true, leapYear.isLeapYear(2000));
+    }
 }
