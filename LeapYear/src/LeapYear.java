1cf67051167e4cb3a50a0147f88d383764bb8129
Deepak Kumar <deepjindl65@gmail.com>
2020-05-15 21:13:52 +0530

    Third Test: Passed

--- LeapYear/src/LeapYear.java
+++ LeapYear/src/LeapYear.java
@@ -6,6 +6,8 @@ public class LeapYear {
     public boolean isLeapYear(int year){
         if (year%400==0){
             return true;
+        }else if(year%4==0 && year%100 !=0 ){
+            return true;
         }
         return false;
     }
